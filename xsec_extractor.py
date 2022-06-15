446bb696a43c4471af22f503c809908879adb31a
José Alfredo Rosas Córdova <jarosascordova@gmail.com>
2022-06-15 19:41:38 +0200

    PNG export

--- xsec_extractor.py
+++ xsec_extractor.py
@@ -1,24 +1,28 @@
 import numpy as np
 import matplotlib.pylab as plt
+import matplotlib.patches as mpatches
 import seaborn as sns
 import ezdxf
 import os
 
 DPI = 300
-COLORS = ["darkblue", "darkred", "darkgreen", "darkorange",
+COLORS = ["black", "darkblue", "darkred", "darkgreen", "darkorange",
           "darkcyan", "darkmagenta", "dimgray", "goldenrod"]
 sns.set_theme(style="whitegrid", font="Palatino Linotype",
               context="paper", palette=COLORS)
 
 DATA_DIR = "meshes/"
-EXPORT_DIR = "dxf_files/"
-for dxf_file in os.listdir(EXPORT_DIR):
-    os.remove(EXPORT_DIR + dxf_file)
+DXF_EXPORT_DIR = "dxf_files/"
+PNG_EXPORT_DIR = "png_files/"
+for dxf_file in os.listdir(DXF_EXPORT_DIR):
+    os.remove(DXF_EXPORT_DIR + dxf_file)
+for png_file in os.listdir(PNG_EXPORT_DIR):
+    os.remove(PNG_EXPORT_DIR + png_file)
 
 # %% Specify .tir file name and main axis
 
-XSECS_FILE = DATA_DIR + "cessna172_ydir.tri"
-NORMAL_AXIS = 'y'
+XSECS_FILE = DATA_DIR + "cessna172.tri"
+NORMAL_AXIS = 'x'
 
 # %% XSec extractor
 
@@ -43,10 +47,10 @@ else:
         xsec_list.append(xsec)
 
 for i, xsec in enumerate(xsec_list):
-    
+
     if np.size(xsec, 0) < 10:
         continue
-    
+
     h_center_val = (np.max(xsec[:, 0]) -
                     np.min(xsec[:, 0]))/2 + np.min(xsec[:, 0])
     v_center_val = (np.max(xsec[:, 1]) -
@@ -65,27 +69,64 @@ for i, xsec in enumerate(xsec_list):
     sorted_xsec_array = polar_array[np.lexsort(
         (polar_array[:, 3], polar_array[:, 2]))]
 
+
+#%% Plot the XSecs
+
     fig = plt.figure(dpi=DPI)
     ax = fig.add_subplot(111)
+    ax.axis('equal')
     if NORMAL_AXIS == 'x':
         ax.set_title("XSec_{0}, x = {1} m".format(
             i, round(locs[i] - locs[0], 3)))
-        ax.axis('equal')
         ax.set_xlabel("y, m")
         ax.set_ylabel("z, m")
     else:
         ax.set_title("XSec {0}, y = {1} m".format(
             i, round(locs[i] - locs[0], 3)))
-        ax.axis('equal')
         ax.set_xlabel("x, m")
         ax.set_ylabel("z, m")
-    ax.plot(sorted_xsec_array[:, 0], sorted_xsec_array[:, 1])
 
-# %% Export .dxf file
+    plot_array = np.vstack((sorted_xsec_array, sorted_xsec_array[0, :]))
+    ax.plot(plot_array[:, 0], plot_array[:, 1], alpha=1)
 
-    doc = ezdxf.new('R2010')
+
+# %% Export XSecs as DXF
+
+    doc = ezdxf.new('R12')
     msp = doc.modelspace()
     for k in range(int(np.size(sorted_xsec_array, 0))-1):
         msp.add_line(sorted_xsec_array[k], sorted_xsec_array[k+1])
     msp.add_line(sorted_xsec_array[-1], sorted_xsec_array[0])
-    doc.saveas(EXPORT_DIR + "XSec_{0}.dxf".format(i))
+
+    msp.add_line((0.2, 0.2), (0.2, -0.2))
+    msp.add_line((0.2, -0.2), (-0.2, -0.2))
+    msp.add_line((-0.2, -0.2), (-0.2, 0.2))
+    msp.add_line((-0.2, 0.2), (0.2, 0.2))
+
+    doc.saveas(DXF_EXPORT_DIR + "XSec_{0}.dxf".format(i))
+
+# %% Export XSecs as PNG
+
+    fig = plt.figure(dpi=DPI)
+    ax = fig.add_subplot(111)
+    ax.grid(False)
+    ax.axis(False)
+    ax.axis('equal')
+    
+    # poly = mpatches.Polygon(sorted_xsec_array[:, :2])
+    # ax.add_patch(poly)
+    
+    plot_array = np.vstack((sorted_xsec_array, sorted_xsec_array[0, :]))
+    ax.plot(plot_array[:, 0], plot_array[:, 1], alpha=1)
+    
+    height = np.max(sorted_xsec_array[:, 1]) - np.min(sorted_xsec_array[:, 1])
+    width = np.max(sorted_xsec_array[:, 0]) - np.min(sorted_xsec_array[:, 0])
+    
+    ax.hlines(0, -width/2, width/2, linestyle="dashed", color="red")
+    ax.vlines(0, -height/2, height/2, linestyle="dashed", color="red")
+    
+    ax.hlines(height/2, -width/2, width/2, linestyle="dotted", color="blue")
+    ax.hlines(-height/2, -width/2, width/2, linestyle="dotted", color="blue")
+    
+    fig.savefig(PNG_EXPORT_DIR + "XSec_{0}.png".format(i),
+        format="png", bbox_inches="tight")
