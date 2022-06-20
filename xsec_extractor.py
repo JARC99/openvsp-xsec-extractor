3ecb5eb4e9e4bdbfe6e49787a5abd54cdddd9e49
José Alfredo Rosas Córdova <jarosascordova@gmail.com>
2022-06-20 22:42:10 +0200

    Better numpy code

--- xsec_extractor.py
+++ xsec_extractor.py
@@ -1,3 +1,4 @@
+# %% Import required modules
 import numpy as np
 import matplotlib.pylab as plt
 import seaborn as sns
@@ -5,8 +6,7 @@ import ezdxf
 import os
 
 DPI = 300
-COLORS = ["black", "darkblue", "darkred", "darkgreen", "darkorange",
-          "darkcyan", "darkmagenta", "dimgray", "goldenrod"]
+COLORS = ["black", "darkblue", "darkred"]
 sns.set_theme(style="whitegrid", font="Palatino Linotype",
               context="paper", palette=COLORS)
 
@@ -20,8 +20,10 @@ for png_file in os.listdir(PNG_EXPORT_DIR):
 
 # %% Specify .tir file name and main axis
 
-XSECS_FILE = DATA_DIR + "cessna172.tri"
-NORMAL_AXIS = 'x'
+XSECS_FILE = DATA_DIR + "pod_and_wing.tri"
+NORMAL_AXIS = 'y'
+
+MIN_POINTS = 5
 
 # %% XSec extractor
 
@@ -32,22 +34,21 @@ if NORMAL_AXIS == 'x':
     locs = np.unique(xsecs_data[:, 0])
 
     xsec_list = []
-    for i in range(len(locs)):
-        xsec = xsecs_data[xsecs_data[:, 0] == locs[i]][:, 1:]
+    for i, loc in enumerate(locs):
+        xsec = xsecs_data[xsecs_data[:, 0] == loc][:, 1:]
         xsec_list.append(xsec)
 
 else:
     locs = np.unique(xsecs_data[:, 1])
 
     xsec_list = []
-    for i in range(len(locs)):
-        xsec = np.stack((xsecs_data[xsecs_data[:, 1] == locs[i]]
-                        [:, 0], xsecs_data[xsecs_data[:, 1] == locs[i]][:, -1]), 1)
+    for i, loc in enumerate(locs):
+        xsec = np.stack((xsecs_data[xsecs_data[:, 1] == loc]
+                        [:, 0], xsecs_data[xsecs_data[:, 1] == loc][:, -1]), 1)
         xsec_list.append(xsec)
 
 for i, xsec in enumerate(xsec_list):
-
-    if np.size(xsec, 0) < 10:
+    if np.size(xsec, 0) < MIN_POINTS:
         continue
 
     h_center_val = (np.max(xsec[:, 0]) -
@@ -55,21 +56,15 @@ for i, xsec in enumerate(xsec_list):
     v_center_val = (np.max(xsec[:, 1]) -
                     np.min(xsec[:, 1]))/2 + np.min(xsec[:, 1])
 
-    cent_xsec_array = np.stack(
-        (xsec[:, 0] - h_center_val, xsec[:, 1] - v_center_val), 1)
+    cent_xsec_array = xsec - np.array([h_center_val, v_center_val])
 
-    angle_array = np.reshape(np.arctan2(
-        cent_xsec_array[:, 1], cent_xsec_array[:, 0]), (int(np.size(xsec, 0)), 1))
+    angle_array = np.arctan2(cent_xsec_array[:, 1], cent_xsec_array[:, 0])
+    dist_array = np.sqrt(cent_xsec_array[:, 0]**2 + cent_xsec_array[:, 1]**2)
 
-    dist_array = np.reshape(np.sqrt(
-        cent_xsec_array[:, 0]**2 + cent_xsec_array[:, 1]**2), (int(np.size(xsec, 0)), 1))
-
-    polar_array = np.hstack((cent_xsec_array, angle_array, dist_array))
-    sorted_xsec_array = polar_array[np.lexsort(
-        (polar_array[:, 3], polar_array[:, 2]))]
+    sorted_xsec_array = cent_xsec_array[np.lexsort((dist_array, angle_array))]
 
 
-#%% Plot the XSecs
+# %% Plot the XSecs
 
     fig = plt.figure(dpi=DPI)
     ax = fig.add_subplot(111)
@@ -106,23 +101,23 @@ for i, xsec in enumerate(xsec_list):
 
 # %% Export XSecs as PNG
 
-    fig = plt.figure(dpi=DPI)
-    ax = fig.add_subplot(111)
-    ax.grid(False)
-    ax.axis(False)
-    ax.axis('equal')
-    
-    plot_array = np.vstack((sorted_xsec_array, sorted_xsec_array[0, :]))
-    ax.plot(plot_array[:, 0], plot_array[:, 1], alpha=1)
-    
-    height = np.max(sorted_xsec_array[:, 1]) - np.min(sorted_xsec_array[:, 1])
-    width = np.max(sorted_xsec_array[:, 0]) - np.min(sorted_xsec_array[:, 0])
-    
-    ax.hlines(0, -width/2, width/2, linestyle="dashed", color="red")
-    ax.vlines(0, -height/2, height/2, linestyle="dashed", color="red")
-    
-    ax.hlines(height/2, -width/2, width/2, linestyle="dotted", color="blue")
-    ax.hlines(-height/2, -width/2, width/2, linestyle="dotted", color="blue")
-    
-    fig.savefig(PNG_EXPORT_DIR + "XSec_{0}.png".format(i),
-        format="png", bbox_inches="tight")
+    # fig = plt.figure(dpi=DPI)
+    # ax = fig.add_subplot(111)
+    # ax.grid(False)
+    # ax.axis(False)
+    # ax.axis('equal')
+
+    # plot_array = np.vstack((sorted_xsec_array, sorted_xsec_array[0, :]))
+    # ax.plot(plot_array[:, 0], plot_array[:, 1], alpha=1)
+
+    # height = np.max(sorted_xsec_array[:, 1]) - np.min(sorted_xsec_array[:, 1])
+    # width = np.max(sorted_xsec_array[:, 0]) - np.min(sorted_xsec_array[:, 0])
+
+    # ax.hlines(0, -width/2, width/2, linestyle="dashed", color="red")
+    # ax.vlines(0, -height/2, height/2, linestyle="dashed", color="red")
+
+    # ax.hlines(height/2, -width/2, width/2, linestyle="dotted", color="blue")
+    # ax.hlines(-height/2, -width/2, width/2, linestyle="dotted", color="blue")
+
+    # fig.savefig(PNG_EXPORT_DIR + "XSec_{0}.png".format(i),
+    #     format="png", bbox_inches="tight")
